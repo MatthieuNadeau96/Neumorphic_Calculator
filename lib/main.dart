eeed31171ab66b053bb8747ba9a3fc787e6f1d31
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-05-04 14:17:31 -0400

    created a list of button strings and made a GridView.builder to display them

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String userQuery = '69 + 65';
+  String userAnswer = '134';
+
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
+
   @override
   Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text('Calculator'),
+      body: Column(
+        children: <Widget>[
+          Spacer(flex: 1),
+          Expanded(
+            flex: 2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  userQuery,
+                  style: TextStyle(fontSize: 40),
+                ),
+                Text(
+                  userAnswer,
+                  style: TextStyle(fontSize: 30, color: themeData.primaryColor),
+                ),
+              ],
+            ),
+          ),
+          Spacer(flex: 1),
+          Expanded(
+            flex: 6,
+            child: GridView.builder(
+              itemCount: buttons.length,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+              itemBuilder: (BuildContext context, int index) {
+                return Text(buttons[index]);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
